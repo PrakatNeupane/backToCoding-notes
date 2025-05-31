626ee43c14152a2e2856f5a03753dab905ca2737
PrakatNeupane <neupaneprakat@gmail.com>
2025-06-01 00:03:08 +1000

    other components

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Notecard from "../../components/Cards/Notecard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt8">
+          <Notecard
+            title="Meeting on 4th April"
+            date="4th April 2025"
+            content="Meeting is bound to happen"
+            tags="#Meeting202"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+
+          <Notecard
+            title="Meeting on 4th April"
+            date="4th April 2025"
+            content="Meeting is bound to happen"
+            tags="#Meeting202"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+          <Notecard
+            title="Meeting on 4th April"
+            date="4th April 2025"
+            content="Meeting is bound to happen"
+            tags="#Meeting202"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+
+          <Notecard
+            title="Meeting on 4th April"
+            date="4th April 2025"
+            content="Meeting is bound to happen"
+            tags="#Meeting202"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+
+          <Notecard
+            title="Meeting on 4th April"
+            date="4th April 2025"
+            content="Meeting is bound to happen"
+            tags="#Meeting202"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
