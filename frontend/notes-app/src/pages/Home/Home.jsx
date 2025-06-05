2cfe1251f0085697be7a93e362f6afc01b6355f6
PrakatNeupane <neupaneprakat@gmail.com>
2025-06-06 07:31:27 +1000

    some stuff

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Notecard from "../../components/Cards/Notecard";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <Navbar />
@@ -19,51 +27,30 @@ const Home = () => {
             onDelete={() => {}}
             onPinNote={() => {}}
           />
-
-          <Notecard
-            title="Meeting on 4th April"
-            date="4th April 2025"
-            content="Meeting is bound to happen"
-            tags="#Meeting202"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
-          <Notecard
-            title="Meeting on 4th April"
-            date="4th April 2025"
-            content="Meeting is bound to happen"
-            tags="#Meeting202"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
-
-          <Notecard
-            title="Meeting on 4th April"
-            date="4th April 2025"
-            content="Meeting is bound to happen"
-            tags="#Meeting202"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
-
-          <Notecard
-            title="Meeting on 4th April"
-            date="4th April 2025"
-            content="Meeting is bound to happen"
-            tags="#Meeting202"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
         </div>
       </div>
+      <button
+        className="cursor-pointer w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes />
+      </Modal>
     </>
   );
 };
