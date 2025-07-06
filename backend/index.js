37471a6631078d851436c73d3d24bb883c870c63
PrakatNeupane <neupaneprakat@gmail.com>
2025-07-06 21:51:45 +1000

    Backend code complete

--- backend/index.js
+++ backend/index.js
@@ -109,6 +109,24 @@ app.post("/login", async (req, res) => {
   }
 });
 
+// Get user
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const isUser = await User.findOne({ _id: user._id });
+
+  if (!isUser) return res.sendStatus(401);
+
+  return res.json({
+    user: {
+      fullName: isUser.fullName,
+      email: isUser.email,
+      _id: isUser._id,
+      createdOn: isUser.createdOn,
+    },
+    message: "User found with above info.",
+  });
+});
+
 // Add note
 app.post("/add-note", authenticateToken, async (req, res) => {
   const { title, tags, content } = req.body || {};
